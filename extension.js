fe541bdc4b7a2147cc6aeb8117414f0d47e13232
David <zwssd1980@163.com>
2017-10-27 11:16:38 +0800

    修改了语言包和调用方式，调用中文语言包成功。

--- extension.js
+++ extension.js
@@ -2,10 +2,17 @@ const St = imports.gi.St;
 const Main = imports.ui.main;
 const Tweener = imports.ui.tweener;
 const Mainloop   = imports.mainloop;
-const Lang = imports.lang;
+
+const Gettext = imports.gettext;
+const _ = Gettext.domain('repetitive-stress-injury').gettext;
+
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
 
 let text, text_time;
 let TIMEOUT_MS = 1000;
+//一个小时，按秒计算，可以自己调整时间
+let maxtime = 10;
 
 function _hideHello() {
     Main.uiGroup.remove_actor(text);
@@ -14,7 +21,10 @@ function _hideHello() {
 
 function _showHello() {
     if (!text) {
-        text = new St.Label({ style_class: 'helloworld-label', text: "Hello, world!" });
+        text = new St.Label({
+            style_class: 'rest-label',
+            text: _("Time is up, please have a rest.")
+        });
         Main.uiGroup.add_actor(text);
     }
 
@@ -32,62 +42,37 @@ function _showHello() {
                        onComplete: _hideHello });
 }
 
-function CurentTime()
-{
-    var now = new Date();
-
-    var hh = now.getHours();            //时
-    var mm = now.getMinutes();          //分
-    var ss = now.getSeconds();          //秒
-
-    var clock = "";
-
-    if(hh < 10)
-        clock += "0";
-
-    clock += hh + ":";
-    if (mm < 10) clock += '0';
-    clock += mm + ":";
-    if (ss < 10) clock += '0';
-    clock += ss;
-    return(clock);
-}
-
-//一个小时，按秒计算，可以自己调整时间
-var maxtime = 10;
-function init() {
-
-    text_time = new St.Label({
-        style_class: 'panel-label'
-    });
+function init(extensionMeta) {
+    let localeDir = Me.dir.get_child('locale');
+    Gettext.bindtextdomain('repetitive-stress-injury', localeDir.get_path());
 
     let msg;
     let minutes;
     let seconds;
+
+    text_time = new St.Label({
+        style_class: 'panel-label'
+    });
 
     this._timeout = Mainloop.timeout_add(TIMEOUT_MS, function () {
         if(maxtime>0)
         {
             minutes = Math.floor(maxtime/60);
             seconds = Math.floor(maxtime%60);
-            msg = "距离结束还有 "+minutes.toString()+" 分 "+seconds.toString()+" 秒";
-            if(maxtime == 5*60) msg = '注意，还有5分钟!';
+            msg = _("The distance is over ")+minutes.toString()+" "+ _("Minutes") +" "+seconds.toString()+" "+_("Seconds");
+            if(maxtime == 5*60) msg = _("Attention, five minutes!");
             maxtime = maxtime-1;
             text_time.text = msg;
             return true;
         }
         else if(maxtime<=0)
         {
-            //clearInterval(timer);
-            //msg = "时间到，结束!"+maxtime.toString();
-            msg = "时间到，结束!";
+            msg = _("Time is up, the countdown is over!");
             _showHello();
             text_time.text = msg;
             return false;
         }
     });
-
-    //text_time.text = msg;
 
     text_time.connect('button-press-event', _showHello);
 }
